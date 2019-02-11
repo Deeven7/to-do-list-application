330feec95c25fa4f4b3570964f7cb25c2aa669b4
Deeven Paul <pauldeeven7@gmail.com>
2019-02-11 13:43:10 -0500

    Fifth Commit

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ Item.find({}, function(err, foundItems){
     });
     res.redirect("/");
   }else{
-    res.render("list", {listTitle: "Today", Additems:foundItems });
+    res.render("list", {listTitle: "Journal", Additems:foundItems });
   }
 
 });
@@ -105,7 +105,7 @@ app.post("/",function(req, res){
   });
 
 
-if(listName === "Today"){
+if(listName === "Journal"){
   item.save();
 res.redirect("/");
 }else{
@@ -122,7 +122,7 @@ app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
-  if(listName === "Today"){
+  if(listName === "Journal"){
     Item.findByIdAndDelete(checkedItemId,function(err){
       if(err){
         console.log("There is an error");
